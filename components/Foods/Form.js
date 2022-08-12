aa084e8c1e3cfbe8b02b17cdc89c0c2cd7848495
tarekkhater <tarekkhater103@gmail.com>
2022-08-12 13:13:35 +0200

    fodd web

--- components/Foods/Form.js
+++ components/Foods/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from '../../styles/Form.module.css';
 export default function Form() {
     
@@ -9,24 +9,24 @@ export default function Form() {
     
   return (
     <div className={styles.container}>    
-        <form id={styles.form} className="row g-3 needs-validation" novalidate>
+        <form id={styles.form} className="row g-3 needs-validation" noValidate>
             <h1>Make Your Order Now!</h1>
           <div className="">
-              <label for="validationCustom01" className="form-label">First name</label>
+              <label htmlFor="validationCustom01" className="form-label">First name</label>
               <input type="text" className="form-control" id="validationCustom01"  required />
                 <div className="valid-feedback">
                   Looks good!
                 </div>
             </div>
             <div className="">
-              <label for="validationCustom02" className="form-label">Last name</label>
+              <label htmlFor="validationCustom02" className="form-label">Last name</label>
               <input type="text" className="form-control" id="validationCustom02"  required / >
                 <div className="valid-feedback">
                   Looks good!
                 </div>
             </div>
             <div className="">
-              <label for="validationCustomUsername" className="form-label">Phone</label>
+              <label htmlFor="validationCustomUsername" className="form-label">Phone</label>
          <div className="input-group has-validation">
           <input type="phone" className="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required / >
              <div className="invalid-feedback">
@@ -35,7 +35,7 @@ export default function Form() {
             </div>
           </div>
           <div className="">
-            <label for="validationCustom03" className="form-label">Adress</label>
+            <label htmlFor="validationCustom03" className="form-label">Adress</label>
             <input type="text" className="form-control" id="validationCustom03" required  />
               <div className="invalid-feedback">
                 Please provide a valid adress.
@@ -44,7 +44,7 @@ export default function Form() {
             <div className="">
             <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required />
-                <label className="form-check-label" for="invalidCheck">
+                <label className="form-check-label" htmlFor="invalidCheck">
                   Agree to terms and conditions
           </label>
              <div className="invalid-feedback">
