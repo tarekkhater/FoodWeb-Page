fcc53bb10e49644cad25c77fd5410a7de1ddd47b
tarekkhater <tarekkhater103@gmail.com>
2022-08-12 13:23:59 +0200

    food

--- components/Foods/Header.js
+++ components/Foods/Header.js
@@ -20,18 +20,18 @@ import Form from './Form';
     
     const Products = () => {
         let [items, setItems] = useState( [
-            { img :item1 , name:"Rainbow Vegetable Sandwich" , Time: "15 - 20 Minutes" , Service : 1 , rightPrice:'$10.50' , oldPrice:'$11.70'},
-            { img :item2 , name:"Vegetarian Burger Sandwich" , Time: "15 - 20 Minutes" , Service : 1 , rightPrice:'$9.20' , oldPrice:'$10.50'},
-            { img :item3 , name:"Raspberry Stuffed French " , Time: "20 - 30 Minutes" , Service : 1 , rightPrice:'$12.50' , oldPrice:'$13.20'},
-            { img :item4 , name:"Summer Salad Sandwich" , Time: "15 - 20 Minutes" , Service : 1 , rightPrice:'$8.20' , oldPrice:'$9.50'},
-            { img :item5 , name:"Fried Chicken Sandwich" , Time: "25 - 30 Minutes" , Service : 1 , rightPrice:'$10.20' , oldPrice:'$12.10'},
-            { img :item6 , name:"Grilled Cheese Sandwich" , Time: "10 - 20 Minutes" , Service : 1 , rightPrice:'$7.50' , oldPrice:'$8.70'},
-            { img :item7 , name:"Vegetarian Pan Banat Sandwich" , Time: "10 - 15 Minutes" , Service : 1 , rightPrice:'$7.50' , oldPrice:'$8.70'},
-            { img :item8 , name:" Butter milk-Fried Chicken " , Time: "25 - 40 Minutes" , Service : 1 , rightPrice:'$10.20' , oldPrice:'$11.50'},
-            { img :item9 , name:"RedHot Spicy Fried Chicken" , Time: "25 - 40 Minutes" , Service : 1 , rightPrice:'$11.50' , oldPrice:'$13.20'},
-            { img :item10 , name:"Air-Fryer Fried Chicken" , Time: "20 - 30 Minutes" , Service : 1 , rightPrice:'$10.00' , oldPrice:'$11.20'},
-            { img :item11 , name:"Hot-Crispy Fried Chicken" , Time: "15 - 20 Minutes" , Service : 1 , rightPrice:'$9.80' , oldPrice:'$10.50'},
-            { img :item12 , name:"Southwest Fried Chicken " , Time: "25 - 30 Minutes" , Service : 1 , rightPrice:'$10.50' , oldPrice:'$12.00'},
+            {id:1 , img :item1 , name:"Rainbow Vegetable Sandwich" , Time: "15 - 20 Minutes" , Service : 1 , rightPrice:'$10.50' , oldPrice:'$11.70'},
+            { id:2 , img :item2 , name:"Vegetarian Burger Sandwich" , Time: "15 - 20 Minutes" , Service : 1 , rightPrice:'$9.20' , oldPrice:'$10.50'},
+            { id:3, img :item3 , name:"Raspberry Stuffed French " , Time: "20 - 30 Minutes" , Service : 1 , rightPrice:'$12.50' , oldPrice:'$13.20'},
+            { id:4 , img :item4 , name:"Summer Salad Sandwich" , Time: "15 - 20 Minutes" , Service : 1 , rightPrice:'$8.20' , oldPrice:'$9.50'},
+            { id:5 , img :item5 , name:"Fried Chicken Sandwich" , Time: "25 - 30 Minutes" , Service : 1 , rightPrice:'$10.20' , oldPrice:'$12.10'},
+            { id:6 , img :item6 , name:"Grilled Cheese Sandwich" , Time: "10 - 20 Minutes" , Service : 1 , rightPrice:'$7.50' , oldPrice:'$8.70'},
+            { id:7 , img :item7 , name:"Vegetarian Pan Banat Sandwich" , Time: "10 - 15 Minutes" , Service : 1 , rightPrice:'$7.50' , oldPrice:'$8.70'},
+            { id:8 , img :item8 , name:" Butter milk-Fried Chicken " , Time: "25 - 40 Minutes" , Service : 1 , rightPrice:'$10.20' , oldPrice:'$11.50'},
+            { id:9 , img :item9 , name:"RedHot Spicy Fried Chicken" , Time: "25 - 40 Minutes" , Service : 1 , rightPrice:'$11.50' , oldPrice:'$13.20'},
+            { id:10 , img :item10 , name:"Air-Fryer Fried Chicken" , Time: "20 - 30 Minutes" , Service : 1 , rightPrice:'$10.00' , oldPrice:'$11.20'},
+            { id:11, img :item11 , name:"Hot-Crispy Fried Chicken" , Time: "15 - 20 Minutes" , Service : 1 , rightPrice:'$9.80' , oldPrice:'$10.50'},
+            { id:12 , img :item12 , name:"Southwest Fried Chicken " , Time: "25 - 30 Minutes" , Service : 1 , rightPrice:'$10.50' , oldPrice:'$12.00'},
        ]);
     
         return (
